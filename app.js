0c7772d7b0832b304574f909010852fec20be9ee
orLevinson <xxxdogerxxx@gmail.com>
2026-05-18 14:43:37 +0300

    commiting the code and proof to github

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('<html><body><h1>GET</h1></body></html>');
+});
+
+app.listen(8080);
